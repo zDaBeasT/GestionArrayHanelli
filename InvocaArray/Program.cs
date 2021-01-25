56832e1ca784fa115bb7978126916a901d8fc42b
Daniele Hanelli <hanellidaniele@gmail.com>
2021-01-25 19:54:50 +0100

    Add project files.

---
+++ InvocaArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LibreriaArray;
+
+namespace InvocaArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quanti elementi vuoi?");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            GestioneArray.LeggiArray(a);
+            int[] nuovoArray = GestioneArray.CopiaArray(a);
+            GestioneArray.StampaArray(nuovoArray);
+            bool trovato = GestioneArray.RicercaArray(a, 4);
+            Console.WriteLine("Trovato: " + trovato);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LibreriaArray/GestioneArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LibreriaArray
+{
+    public class GestioneArray
+    {
+        public static void LeggiArray(int[] array)
+        {
+            Console.WriteLine($"Inserisci {array.Length} elementi");
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Inserisci un numero: ");
+                int n = int.Parse(Console.ReadLine());
+                array[i] = n;
+            }
+        }
+
+        public static void StampaArray(int[] array)
+        {
+            Console.Write("L'array contiene: ");
+            for (int i = 0; i< array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+
+        public static int[] CopiaArray(int[] array)
+        {
+            int[] newArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            return newArray;
+        }
+
+        public static bool RicercaArray(int[] array, int chiave)
+        {
+            bool trovato = false;
+            for (int i = 0; i < array.Length && !trovato; i++)
+            {
+                if (array[i] == chiave)
+                {
+                    trovato = true;
+                }
+            }
+            return trovato;
+        }
+    }
+}
